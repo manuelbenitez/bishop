498a19efa64e1ca4762b3e90e549aa7e7e48a591
Manuel <manu.benitez1@hotmail.com>
2022-09-11 18:49:23 +0200

    refactor: input

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../ui/input";
 import { ISigninProps } from "./signin.types";
-
+import styles from "./signin.module.scss";
+import { BodyFive } from "../../ui/typography/common";
+import { wildBlueYonder } from "../../ui/typography/common/colors";
 const Singin = ({ isSignedIn }: ISigninProps) => {
-  return <div>Singin</div>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  return (
+    <div className={styles["container"]}>
+      <BodyFive color={wildBlueYonder} label={"Singin"} />
+      <Input
+        label={"email"}
+        value={email}
+        onChange={setEmail}
+        placeholder={"Enter your email"}
+      />
+      <Input
+        label={"password"}
+        value={password}
+        onChange={setPassword}
+        placeholder={"Enter your password"}
+      />
+    </div>
+  );
 };
 
 export default Singin;
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../ui/input";
+import { BodyFive } from "../../ui/typography/common";
+import { wildBlueYonder } from "../../ui/typography/common/colors";
 import styles from "./signup.module.scss";
 import { ISignupProps } from "./signup.types";
 const Signup = ({ isSignedUp }: ISignupProps) => {
-  return <div className={styles["container"]}>Signup</div>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  return (
+    <div className={styles["container"]}>
+      <BodyFive color={wildBlueYonder} label={"Signup"} />
+      <Input
+        label={"email"}
+        value={email}
+        onChange={setEmail}
+        placeholder={"Enter your email"}
+      />
+      <Input
+        label={"password"}
+        value={password}
+        onChange={setPassword}
+        placeholder={"Enter your password"}
+      />
+    </div>
+  );
 };
 
 export default Signup;
